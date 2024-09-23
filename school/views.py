0cc97ab6858b13ece5e53c54e771f92f190a1c55
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-09-22 20:11:14 -0400

    Adjusting permissions in views. Removing unused view.

--- school/views.py
+++ school/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import generics
+from rest_framework import status, permissions, generics
 from .models import GroupModel, StudentModel, SubjectModel, ExamModel
 from .serializers import GroupModelSerializer, \
                   StudentModelSerializer, SubjectModelSerializer,ExamModelSerializer
@@ -11,10 +10,12 @@ from rest_framework.pagination import PageNumberPagination
 class GroupListCreateView(generics.ListCreateAPIView):
     queryset = GroupModel.objects.all()
     serializer_class = GroupModelSerializer   
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class GroupRetrieveUpdateDestroView(generics.RetrieveUpdateDestroyAPIView):
     queryset = GroupModel.objects.all()
     serializer_class = GroupModelSerializer   
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class StudentPagination(PageNumberPagination):
     page_size = 10
@@ -25,19 +26,32 @@ class StudentListCreateView(generics.ListCreateAPIView):
     queryset = StudentModel.objects.all()
     serializer_class = StudentModelSerializer   
     pagination_class=StudentPagination
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class StudentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = StudentModel.objects.prefetch_related('exams')
     serializer_class = StudentModelSerializer   
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class SubjectListCreateView(generics.ListCreateAPIView):
     queryset = SubjectModel.objects.all()
     serializer_class = SubjectModelSerializer   
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+class SubjectRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SubjectModel.objects.all()
+    serializer_class = SubjectModelSerializer  
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class ExamListCreateView(generics.ListCreateAPIView):
     queryset = ExamModel.objects.all()
-    serializer_class = ExamModelSerializer     
+    serializer_class = ExamModelSerializer   
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] 
+
+class ExamRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ExamModel.objects.all()
+    serializer_class = ExamModelSerializer   
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 @api_view()
 def students_by_group(request):
@@ -46,17 +60,6 @@ def students_by_group(request):
     data = [{'group_name':group.name, 'student_count':group.student_count} for group in groups]
 
     return Response(data,status=status.HTTP_200_OK)
-
-@api_view()
-def average_by_group(request, date):
-    groups = GroupModel.get_groups_with_average_score(date)
-
-    print(groups)
-
-    data = [{'group_name':group.name, 'average_score':group.average_score} for group in groups]
-
-    return Response(data,status=status.HTTP_200_OK)
-
 
 @api_view()
 def top_student_by_group(request, group_id):
